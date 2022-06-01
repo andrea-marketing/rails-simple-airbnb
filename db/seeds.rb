c8a1113a6197d9bf99ac58bdc6c2c542dbdb6819
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-06-01 17:01:26 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database'
+Flat.destroy_all
+puts 'Creating restaurants'
+
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::Games::SuperMario.game,
+    address: Faker::Address.street_address,
+    description: Faker::TvShows::FamilyGuy.quote,
+    price_per_night: rand(40..300),
+    number_of_guests: rand(0..10)
+  )
+  flat.save
+end
+
+puts "Finished creating restaurants"
